d9f615c94e29bbe62e80e7dbf0ae637be7556ff2
chakerfezai <fezaichaker@gmail.com>
2024-06-10 08:07:54 +0200

    cart account

--- article-service-ui/src/main/webui/src/app/navbar/navbar.component.ts
+++ article-service-ui/src/main/webui/src/app/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, Input, OnInit} from '@angular/core';
+import {CartService} from "../service/cart.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  @Input() cartCount:number=0;
+
+  private cartService = inject(CartService);
+  cartItems = this.cartService.cartItems();
+  totalItems = computed(() => this.cartItems.reduce((sum, item) => sum + item.quantity, 0));
+
+  ngOnInit(): void {
+    this.cartCount = this.totalItems();
+  }
+
 
 }
