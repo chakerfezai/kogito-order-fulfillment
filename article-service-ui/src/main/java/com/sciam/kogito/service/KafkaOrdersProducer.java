8a2bf4b5219a2e391a80cc14538895af928106fc
chakerfezai <fezaichaker@gmail.com>
2025-04-13 08:11:59 +0200

    new files

--- article-service-ui/src/main/java/com/sciam/kogito/service/KafkaOrdersProducer.java
+++ article-service-ui/src/main/java/com/sciam/kogito/service/KafkaOrdersProducer.java
@@ -34,16 +34,18 @@ public class KafkaOrdersProducer {
 
     public void publishOrder(Order order) {
         OutgoingCloudEventMetadata<Object> eventMetadata = null;
+        log.info("Start Publishing request order {}, metadata {}",order , eventMetadata);
         try {
             eventMetadata = OutgoingCloudEventMetadata.builder()
-                    .withId("id-" +order.getTransactionId())
+                    .withId("id-" + order.getTransactionId())
                     .withSpecVersion("1.0")
                     .withTimestamp(ZonedDateTime.now())
                     .withSource(new URI("orders-start-in"))
                     .withType("orders-start-in")
                     .build();
             orderEmitter.send(Message.of(order).addMetadata(eventMetadata));
-        }catch (Exception exception) {
+            log.info("Publishing request order {}, metadata {}", order, eventMetadata);
+        } catch (Exception exception) {
             throw new RuntimeException();
         }
 
@@ -51,9 +53,10 @@ public class KafkaOrdersProducer {
 
     public void publishPayment(Payment payment) {
         OutgoingCloudEventMetadata<Object> eventMetadata = null;
+        log.info("Start Publishing request payment {}, metadata {}", payment);
         try {
             eventMetadata = OutgoingCloudEventMetadata.builder()
-                    .withId("id-" +payment.getTransactionId())
+                    .withId("id-" + payment.getTransactionId())
                     .withSpecVersion("1.0")
                     .withTimestamp(ZonedDateTime.now())
                     .withSource(new URI("orders-payment-in"))
@@ -63,18 +66,18 @@ public class KafkaOrdersProducer {
             payment.setPaymentStatus(PaymentStatus.PENDING);
             log.info("Publishing request payment {}, metadata {}", payment, eventMetadata);
             paymentEmitter.send(Message.of(payment).addMetadata(eventMetadata));
-        }catch (Exception exception) {
+        } catch (Exception exception) {
             throw new RuntimeException();
         }
     }
 
 
-    public void
-    publishShipping(Shipping shipping) {
+    public void publishShipping(Shipping shipping) {
         OutgoingCloudEventMetadata<Object> eventMetadata = null;
+        log.info("Start Publishing request shipping {}, metadata {}", shipping, eventMetadata);
         try {
             eventMetadata = OutgoingCloudEventMetadata.builder()
-                    .withId("id-" +shipping.getOrderId())
+                    .withId("id-" + shipping.getOrderId())
                     .withSpecVersion("1.0")
                     .withTimestamp(ZonedDateTime.now())
                     .withSource(new URI("orders-shipping-in"))
@@ -83,7 +86,7 @@ public class KafkaOrdersProducer {
 
             log.info("Publishing request shipping {}, metadata {}", shipping, eventMetadata);
             shippingEmitter.send(Message.of(shipping).addMetadata(eventMetadata));
-        }catch (Exception exception) {
+        } catch (Exception exception) {
             throw new RuntimeException();
         }
     }
