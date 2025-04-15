e9f800b2993e087d1173d4a37e505b7ff7199730
chakerfezai <fezaichaker@gmail.com>
2025-04-15 18:27:48 +0200

    clean version of order service

--- order-service/src/main/java/com/sciam/kogito/order/mapper/OrderMapper.java
+++ order-service/src/main/java/com/sciam/kogito/order/mapper/OrderMapper.java
@@ -3,9 +3,11 @@ package com.sciam.kogito.order.mapper;
 
 import com.sciam.kogito.order.dto.OrderDto;
 import com.sciam.kogito.order.model.Order;
+import jakarta.enterprise.context.ApplicationScoped;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 
-@Mapper(config = OrderMappingConfig.class)
+@Mapper(componentModel = "cdi")
 public interface OrderMapper {
 
     OrderDto toOrderDto(Order order);
